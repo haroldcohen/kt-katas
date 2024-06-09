b5c76ea69ecc9357ca791ecefa81241e32ace383
Harold Cohen <h.cohen.apple@me.com>
2024-06-09 04:15:09 +0200

    feat(bowling): finish bowling game kata

--- src/main/kotlin/bowling/BowlingGame.kt
+++ src/main/kotlin/bowling/BowlingGame.kt
@@ -3,32 +3,84 @@ package com.haroldcohen.bowling
 class BowlingGame(
     private var score: Int = 0,
     private var frames: Array<IntArray> = Array(10) {IntArray(2) {0}},
+    private var lastFrame: IntArray = IntArray(3){0},
     private var currentRoll: Int = 0,
-    private var currentFrame: Int = 0,
+    private var currentFrameIndex: Int = 0,
 ) {
 
+    private companion object {
+        fun rollIsStrike(frame: IntArray): Boolean {
+            return frame[0] == 10
+        }
+    }
+
     fun roll(pinsKnockedDown: Int) {
-        frames[currentFrame][currentRoll] = pinsKnockedDown
-        when (currentRoll) {
-            0 -> currentRoll = 1
-            1 -> {
+        when (currentRoll){
+            0 -> firstRoll(pinsKnockedDown)
+            1 -> secondRoll(pinsKnockedDown)
+            2 -> thirdRoll(pinsKnockedDown)
+        }
+    }
+
+    private fun firstRoll(pinsKnockedDown: Int) {
+        currentFrame()[0] = pinsKnockedDown
+        currentRoll++
+        if (rollIsStrike(frame = frames[currentFrameIndex])) strike()
+    }
+
+    private fun secondRoll(pinsKnockedDown: Int) {
+        currentFrame()[1] = pinsKnockedDown
+        when(currentFrameIndex){
+            9 -> currentRoll++
+            else ->  {
                 currentRoll = 0
-                currentFrame++
+                currentFrameIndex++
+            }
+        }
+    }
+
+    private fun thirdRoll(pinsKnockedDown: Int) {
+        currentFrame()[2] = pinsKnockedDown
+    }
+
+    private fun currentFrame(): IntArray {
+        if (currentFrameIndex != 9){
+            return frames[currentFrameIndex]
+        }
+        return lastFrame
+    }
+
+    private fun strike() {
+        when (currentFrameIndex){
+            9 -> {}
+            else -> {
+                currentRoll = 0
+                currentFrameIndex++
             }
         }
     }
 
     private fun computeScore() {
         for (frameNumber in frames.indices) {
-            computeFrameScore(frameNumber = frameNumber)
+            score += when(frameNumber) {
+                9 -> computeLastFrameScore()
+                else -> computeFrameScore(frameNumber = frameNumber)
+            }
         }
     }
 
-    private fun computeFrameScore(frameNumber: Int) {
+    private fun computeFrameScore(frameNumber: Int): Int {
         val nextFrameNumber: Int = frameNumber + 1
-        score += frames[frameNumber][0]
-        score += frames[frameNumber][1]
-        if (score == 10) score += (frames[nextFrameNumber][0] + frames[nextFrameNumber][1])
+        val frameScore = frames[frameNumber].sum()
+        if (rollIsStrike(frame = frames[frameNumber])) return frameScore + frames[nextFrameNumber].sum()
+        if (frameScore == 10) return frameScore + frames[nextFrameNumber][0]
+        return frameScore
+    }
+
+    private fun computeLastFrameScore(): Int {
+        val twoFirstRollsScore = lastFrame.slice(0..1).sum()
+        if (rollIsStrike(frame = lastFrame) or (twoFirstRollsScore == 10)) return lastFrame.sum()
+        return twoFirstRollsScore
     }
 
     fun toSnapshot() : BowlingGameSnapshot {
--- src/test/kotlin/bowling/BowlingGameTests.kt
+++ src/test/kotlin/bowling/BowlingGameTests.kt
@@ -6,7 +6,7 @@ import com.haroldcohen.bowling.BowlingGame
 import com.haroldcohen.bowling.BowlingGameSnapshot
 import io.kotest.matchers.shouldBe
 
-class BowlingGameTests : FunSpec ({
+class BowlingGameTests : FunSpec({
 
     var game = BowlingGame()
 
@@ -15,7 +15,7 @@ class BowlingGameTests : FunSpec ({
     }
 
     test(name = "Knock down 0 pins should score 0") {
-        for (frameNumber in 0..9){
+        for (frameNumber in 0..9) {
             game.roll(pinsKnockedDown = 0)
             game.roll(pinsKnockedDown = 0)
         }
@@ -26,7 +26,7 @@ class BowlingGameTests : FunSpec ({
     test(name = "Knock down 2 pins should score 2") {
         game.roll(pinsKnockedDown = 1)
         game.roll(pinsKnockedDown = 1)
-        for (frameNumber in 0..8){
+        for (frameNumber in 0..8) {
             game.roll(pinsKnockedDown = 0)
             game.roll(pinsKnockedDown = 0)
         }
@@ -35,11 +35,11 @@ class BowlingGameTests : FunSpec ({
     }
 
     test(name = "Knock down 2 pins twice should score 4") {
-        for (frameNumber in 0..1){
+        for (frameNumber in 0..1) {
             game.roll(pinsKnockedDown = 1)
             game.roll(pinsKnockedDown = 1)
         }
-        for (frameNumber in 0..7){
+        for (frameNumber in 0..7) {
             game.roll(pinsKnockedDown = 0)
             game.roll(pinsKnockedDown = 0)
         }
@@ -47,20 +47,46 @@ class BowlingGameTests : FunSpec ({
         game.toSnapshot() shouldBe expectedGame
     }
 
+    test(name = "Knock down 2 pins in the last frame should score 2") {
+        for (frameNumber in 0..9) {
+            when(frameNumber) {
+                9 -> {
+                    game.roll(pinsKnockedDown = 1)
+                    game.roll(pinsKnockedDown = 1)
+                }
+
+                else -> {
+                    game.roll(pinsKnockedDown = 0)
+                    game.roll(pinsKnockedDown = 0)
+                }
+            }
+        }
+        val expectedGame = BowlingGameSnapshot(score = 2)
+        game.toSnapshot() shouldBe expectedGame
+    }
+
     mapOf(
-        0 to 12,
-    ).forEach { (spareFrame, expectedScore) ->
-        test(name = "Roll a spare on the 1st frame and roll 1 in the next frame should score $expectedScore") {
+        0 to 1,
+        1 to 2,
+        2 to 3,
+    ).forEach { (spareFrame, rollFollowingSpare) ->
+        val expectedScore = 10 + rollFollowingSpare * 2
+        test(
+            name = "Roll a spare in frame #${spareFrame + 1} " +
+                    "and then knock down $rollFollowingSpare pins should score $expectedScore"
+        ) {
             for (i in 0..9) {
-                when (i){
+                when (i) {
                     spareFrame -> {
-                        game.roll(5)
-                        game.roll(5)
+                        game.roll(pinsKnockedDown = 5)
+                        game.roll(pinsKnockedDown = 5)
                     }
+
                     spareFrame + 1 -> {
-                        game.roll(0)
-                        game.roll(1)
+                        game.roll(pinsKnockedDown = rollFollowingSpare)
+                        game.roll(pinsKnockedDown = 0)
                     }
+
                     else -> {
                         game.roll(pinsKnockedDown = 0)
                         game.roll(pinsKnockedDown = 0)
@@ -70,6 +96,136 @@ class BowlingGameTests : FunSpec ({
             val expectedGame = BowlingGameSnapshot(score = expectedScore)
             game.toSnapshot() shouldBe expectedGame
         }
+    }
+
+    mapOf(
+        0 to intArrayOf(1, 1),
+        1 to intArrayOf(1, 2),
+        2 to intArrayOf(3, 2),
+    ).forEach { (strikeFrame, rollsFollowingStrike) ->
+        val expectedScore = 10 + rollsFollowingStrike.sum() * 2
+        val roll1 = rollsFollowingStrike[0]
+        val roll2 = rollsFollowingStrike[1]
+        test(
+            name = "Roll a strike in frame #${strikeFrame + 1}, " +
+                    "knock down ${rollsFollowingStrike[0]} pins" +
+                    "and then ${rollsFollowingStrike[1]} pins should score $expectedScore",
+        ) {
+            for (i in 0..9) {
+                when (i) {
+                    strikeFrame -> {
+                        game.roll(pinsKnockedDown = 10)
+                    }
+
+                    strikeFrame + 1 -> {
+                        game.roll(pinsKnockedDown = roll1)
+                        game.roll(pinsKnockedDown = roll2)
+                    }
+
+                    else -> {
+                        game.roll(pinsKnockedDown = 0)
+                        game.roll(pinsKnockedDown = 0)
+                    }
+                }
+            }
+            val expectedGame = BowlingGameSnapshot(score = expectedScore)
+            game.toSnapshot() shouldBe expectedGame
+        }
+    }
+
+    test(name = "Knock down 10 pins on the 2nd roll of frame #1 then knock down 1 pin should score 11") {
+        for (i in 0..9) {
+            when (i) {
+                0 -> {
+                    game.roll(pinsKnockedDown = 0)
+                    game.roll(pinsKnockedDown = 10)
+                }
+
+                1 -> {
+                    game.roll(pinsKnockedDown = 1)
+                    game.roll(pinsKnockedDown = 0)
+                }
+
+                else -> {
+                    game.roll(pinsKnockedDown = 0)
+                    game.roll(pinsKnockedDown = 0)
+                }
+            }
+        }
+        val expectedGame = BowlingGameSnapshot(score = 12)
+        game.toSnapshot() shouldBe expectedGame
+    }
+
+
+    listOf(
+        intArrayOf(1, 1),
+        intArrayOf(10, 10),
+    ).forEach{
+        val expectedScore = 10 + it.sum()
+        test(name = "Roll a strike in the tenth frame should award the player with 2 extra balls" +
+                "and score $expectedScore") {
+            for (i in 0..9) {
+                when (i) {
+                    9 -> {
+                        game.roll(pinsKnockedDown = 10)
+                        game.roll(pinsKnockedDown = it[0])
+                        game.roll(pinsKnockedDown = it[1])
+                    }
+
+                    else -> {
+                        game.roll(pinsKnockedDown = 0)
+                        game.roll(pinsKnockedDown = 0)
+                    }
+                }
+            }
+            val expectedGame = BowlingGameSnapshot(score = expectedScore)
+            game.toSnapshot() shouldBe expectedGame
+        }
+    }
+
+    listOf(
+        5,
+        7,
+    ).forEach{
+        val expectedScore = 10 + it
+        test(name = "Roll a spare in the tenth frame should award the player with 1 extra ball" +
+                "and score $expectedScore") {
+            for (i in 0..9) {
+                when (i) {
+                    9 -> {
+                        game.roll(pinsKnockedDown = 5)
+                        game.roll(pinsKnockedDown = 5)
+                        game.roll(pinsKnockedDown = it)
+                    }
+
+                    else -> {
+                        game.roll(pinsKnockedDown = 0)
+                        game.roll(pinsKnockedDown = 0)
+                    }
+                }
+            }
+            val expectedGame = BowlingGameSnapshot(score = expectedScore)
+            game.toSnapshot() shouldBe expectedGame
+        }
+    }
+
+    test(name = "Roll an open frame in the tenth frame should not award the player with one extra ball"){
+        for (i in 0..9) {
+            when (i) {
+                9 -> {
+                    game.roll(pinsKnockedDown = 5)
+                    game.roll(pinsKnockedDown = 2)
+                    game.roll(pinsKnockedDown = 2)
+                }
+
+                else -> {
+                    game.roll(pinsKnockedDown = 0)
+                    game.roll(pinsKnockedDown = 0)
+                }
+            }
+        }
+        val expectedGame = BowlingGameSnapshot(score = 7)
+        game.toSnapshot() shouldBe expectedGame
     }
 
 })
