26ac6d99ea4c182ae9db73708c9830257e15d173
Harold Cohen <h.cohen.apple@me.com>
2024-06-12 02:34:44 +0200

    feat(bowling): update bowling kata variant

--- src/main/kotlin/bowling/BowlingGame.kt
+++ src/main/kotlin/bowling/BowlingGame.kt
@@ -1,14 +1,25 @@
 package com.haroldcohen.bowling
 
 class BowlingGame(
-    private var rolls: IntArray,
+    private var rolls: IntArray = IntArray(20) { 0 },
     private var currentRollIndex: Int = 0,
 ) {
     fun roll(pinsKnockedDown: Int){
-        rolls[currentRollIndex++] = pinsKnockedDown
+        rolls[currentRollIndex] = pinsKnockedDown
+        when(currentRollIndex){
+            0, 1 -> currentRollIndex++
+            else -> {
+                if(previousRollIsSpare()) rolls[currentRollIndex - 2] += pinsKnockedDown
+                currentRollIndex++
+            }
+        }
     }
 
-    fun toSnapshot(): BowlingGameSnapshot{
+    private fun previousRollIsSpare(): Boolean{
+        return rolls[currentRollIndex - 2] + rolls[currentRollIndex - 1] == 10
+    }
+
+    fun toSnapshot(): BowlingGameSnapshot {
         return BowlingGameSnapshot(score = rolls.sum())
     }
 }
--- src/test/kotlin/bowling/BowlingGameTests.kt
+++ src/test/kotlin/bowling/BowlingGameTests.kt
@@ -7,25 +7,61 @@ import com.haroldcohen.bowling.BowlingGameSnapshot
 import io.kotest.matchers.shouldBe
 
 class BowlingGameTests : FunSpec({
-    test(name = "Knock 0 pins should score 0"){
-        val game = BowlingGame(rolls = IntArray(20))
-        val expectedGame = BowlingGameSnapshot()
-        for (i in 0..9){
-            game.roll(pinsKnockedDown = 0)
+
+    var game = BowlingGame()
+
+    beforeEach(){
+        game = BowlingGame()
+    }
+
+    fun rollSpare(){
+        game.roll(pinsKnockedDown = 5)
+        game.roll(pinsKnockedDown = 5)
+    }
+
+    fun rollN(n: Int, pinsKnockedDown: Int){
+        for (i in 0 until n){
+            game.roll(pinsKnockedDown = pinsKnockedDown)
         }
+    }
+
+    test(name = "Knock 0 pins should score 0"){
+        val expectedGame = BowlingGameSnapshot()
+        rollN(n = 20, pinsKnockedDown = 0)
         game.toSnapshot() shouldBe expectedGame
     }
 
-    test(name = "Knock 2 pins should score 2"){
-        val game = BowlingGame(rolls = IntArray(20))
-        val expectedGame = BowlingGameSnapshot(score = 2)
-        for (i in 0..9){
-            when (i){
-                0 -> game.roll(pinsKnockedDown = 1)
-                1 -> game.roll(pinsKnockedDown = 1)
-                else -> game.roll(pinsKnockedDown = 0)
-            }
-        }
+    test(name = "Knock 20 pins should score 20"){
+        val expectedGame = BowlingGameSnapshot(score = 20)
+        rollN(n = 20, pinsKnockedDown = 1)
         game.toSnapshot() shouldBe expectedGame
     }
+
+    listOf(
+        5,
+        6,
+    ).forEach { rollFollowingSpare ->
+        val expectedScore = 10 + rollFollowingSpare * 2
+        test(name = "Roll a spare should score $expectedScore") {
+            val expectedGame = BowlingGameSnapshot(score = expectedScore)
+            rollSpare()
+            rollN(n = 1, pinsKnockedDown = rollFollowingSpare)
+            rollN(n = 17, pinsKnockedDown = 0)
+            game.toSnapshot() shouldBe expectedGame
+        }
+    }
+
+    listOf(
+        5,
+    ).forEach { rollFollowingSpare ->
+        val expectedScore = 10 + rollFollowingSpare*2 + 17
+        test(name = "Roll a spare should score $expectedScore") {
+            val expectedGame = BowlingGameSnapshot(score = expectedScore)
+            rollN(n = 17, pinsKnockedDown = 1)
+            rollSpare()
+            rollN(n = 1, pinsKnockedDown = rollFollowingSpare)
+            game.toSnapshot() shouldBe expectedGame
+        }
+    }
+
 })
