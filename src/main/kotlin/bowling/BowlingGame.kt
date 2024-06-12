51777d908bd4417695faabd7dafc81468554b1d8
Harold Cohen <h.cohen.apple@me.com>
2024-06-12 02:44:07 +0200

    feat(bowling): refactor roll method

--- src/main/kotlin/bowling/BowlingGame.kt
+++ src/main/kotlin/bowling/BowlingGame.kt
@@ -6,16 +6,12 @@ class BowlingGame(
 ) {
     fun roll(pinsKnockedDown: Int){
         rolls[currentRollIndex] = pinsKnockedDown
-        when(currentRollIndex){
-            0, 1 -> currentRollIndex++
-            else -> {
-                if(previousRollIsSpare()) rolls[currentRollIndex - 2] += pinsKnockedDown
-                currentRollIndex++
-            }
-        }
+        if (previousRollIsSpare()) rolls[currentRollIndex - 2] += pinsKnockedDown
+        currentRollIndex++
     }
 
     private fun previousRollIsSpare(): Boolean{
+        if (currentRollIndex in 0..1) return false
         return rolls[currentRollIndex - 2] + rolls[currentRollIndex - 1] == 10
     }
 
